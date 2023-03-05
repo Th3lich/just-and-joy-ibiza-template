2075b9627206bad00dd3c294a08d1b39d15ee4b6
Th3lich <javilpx2.jl@gmail.com>
2023-03-05 11:29:05 +0100

    Added JavaScript function to resize YouTube video dimensions depending on the screen width

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,42 @@
+
+this.calculateYoutubeLayout = () => {
+    const screenWidth = window.innerWidth;
+
+    let layoutInfo = {
+        height: 122,
+        top: 11,
+    }
+  
+    if (screenWidth > 1158) {
+        const heightSlope = (200 - 160) / (2289 - 1833);
+        const heightIntercept = 200 - heightSlope * 2289;
+        layoutInfo.height = Math.floor(heightSlope * screenWidth + heightIntercept);
+        
+        const topSlope = (51 - 26) / (2301 - 1730);
+        const topIntercept = 51 - topSlope * 2301;
+        layoutInfo.top  = Math.floor(topSlope * screenWidth + topIntercept);
+    }
+  
+    return layoutInfo;
+}
+
+this.calculateYoutubeContainerHeight = () => {
+    const screenWidth = window.innerWidth;
+    let containerHeight = 645;
+
+    if (screenWidth < 1158) {
+        const slope = (570 - 425) / (1030 - 767);
+        const intercept = 570 - slope * 1030;
+        containerHeight = Math.floor(slope * screenWidth + intercept);
+    }
+
+    return containerHeight;
+}
+
+
 window.addEventListener('load', function() {
     // Navbar
-    let navbar = document.querySelector('.navbar');
+    let navbar = this.document.querySelector('.navbar');
 
     window.addEventListener('scroll', function() {
         navbar.classList.toggle('opacity-0', window.scrollY < 150);
@@ -10,4 +46,17 @@ window.addEventListener('load', function() {
         navbar.classList.remove('opacity-0');
     }
 
+    // Youtube video
+    let video = this.document.querySelector('.youtube');
+    video.style.height = `${this.calculateYoutubeLayout().height}%`;
+    video.style.top = `-${this.calculateYoutubeLayout().top}%`;
+
+    let videoSection = this.document.querySelector('.video-section');
+    videoSection.style.height = `${this.calculateYoutubeContainerHeight()}px`;
+
+    this.window.addEventListener('resize', () => {
+        video.style.height = `${this.calculateYoutubeLayout().height}%`;
+        video.style.top = `-${this.calculateYoutubeLayout().top}%`;
+        videoSection.style.height = `${this.calculateYoutubeContainerHeight()}px`;
+    });
 });
